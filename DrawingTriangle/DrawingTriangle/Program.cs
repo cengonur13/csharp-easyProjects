61fe93f9722f2fe18c340c372358bf201b344a6c
Onur <88563126+cengonur13@users.noreply.github.com>
2024-05-12 23:47:20 +0300

    Drawing a Triangle C# Console App
    Added C# Console App that draws a triangle according to the entered size...

---
+++ DrawingTriangle/DrawingTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class DrawingTriangle
+{
+    public static void DrawTri(int size)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                Console.Write("* ");
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Cizilecek ucgenin boyutunu giriniz :");
+        try
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            DrawingTriangle.DrawTri(size);
+        } catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Hatali veri girisi nedeniyle cizim yapilamamistir...");
+        }
+    }
+}

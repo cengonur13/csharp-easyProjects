12e78320427d761dfd0a827679143aa04353a545
Onur <88563126+cengonur13@users.noreply.github.com>
2024-05-13 15:19:22 +0300

    String Algorithm Example C# Console App
    C# Console App that extracts the relevant index value of the data in the input in (data,index) format and returns the new version

---
+++ StringAlgorithm/StringAlgorithm/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class AlgorithmString
+{
+    public static void EditString(string input, int index)
+    {
+        if(index < 0 || index >= input.Length)
+        {
+            Console.WriteLine("Hatalı index girilmistir...");
+            return;
+        }
+        string newString = input.Remove(index,1);
+        Console.WriteLine(newString);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Lutfen ilgili metni ve index degerini arada , ile giriniz :");
+        Console.Write("(data,index) -> ");
+        string input = Console.ReadLine();
+
+        string[] newArr = input.Split(',');
+        /*
+         input : (OnurTas,3) -> output : (OnuTas)
+               : (HelloWorld,5)        : (Helloorld)
+         */
+        if(newArr.Length == 2 && int.TryParse(newArr[1], out int index))
+            AlgorithmString.EditString(newArr[0], index);
+        else
+        {
+            Console.WriteLine("Hatalı formatta veri girisi yapilmistir...");
+        }
+    }
+}

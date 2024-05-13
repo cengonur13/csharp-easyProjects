5c0934f15d78f0d25b15ec6d591c04632d9357c2
Onur <88563126+cengonur13@users.noreply.github.com>
2024-05-13 15:48:53 +0300

    Reverse String C# Console App
    C# Console App that reverse the input string according to the entered string

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class ReverseString
+{
+    public static void ReversingInput(string arr)
+    {
+        string[] newArr = arr.Split(' ');
+
+        for (int i = 0; i < newArr.Length; i++)
+        {
+            char[] temp = newArr[i].ToCharArray();
+            Array.Reverse(temp);
+            newArr[i] = new string(temp);
+
+        }
+
+        foreach (var item in newArr)
+            Console.Write(item + " ");
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("input : (Hello World) output : (olleH dlroW)");
+            Console.WriteLine("--------------------------");
+            Console.Write("Lutfen ters cevrilecek metni giriniz : ");
+            
+            string input = Console.ReadLine();
+
+            ReverseString.ReversingInput(input);
+        }
+    }
+}

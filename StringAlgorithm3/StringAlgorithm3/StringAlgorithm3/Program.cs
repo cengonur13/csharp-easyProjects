afa33284d22f8b4d16c998812ba3a3d98b878b39
Onur <88563126+cengonur13@users.noreply.github.com>
2024-05-13 18:12:08 +0300

    String Algorithm 3 C# Console App
    C# Console App that returns true if the words in the entered sentence have 2 consonants next to each other, false otherwise

---
+++ StringAlgorithm3/StringAlgorithm3/StringAlgorithm3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lutfen ilgili cumleyi giriniz :");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+
+        foreach (var word in words)
+        {
+            bool hasConsecutiveConsonants = CheckMethod(word);
+            Console.Write(hasConsecutiveConsonants + " ");
+        }
+    }
+
+    static bool CheckMethod(string word)
+    {
+        for (int i = 0; i < word.Length - 1; i++)
+        {
+            if (IsConsonant(word[i]) && IsConsonant(word[i + 1]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static bool IsConsonant(char c)
+    {
+        return "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".Contains(c);
+    }
+}

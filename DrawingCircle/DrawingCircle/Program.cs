477c13c679f6c065c81684ad2fd6e2cccf6ec3aa
Onur <88563126+cengonur13@users.noreply.github.com>
2024-05-13 14:47:23 +0300

    Drawing a Circle C# Console App
    C# Console App that draws a circle according to the entered radius...

---
+++ DrawingCircle/DrawingCircle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class DrawingCircle
+{
+    public static void DrawCircle(int r)
+    {
+        for (int i = -r; i <= r; i++)
+        {
+            for(int j = -r; j <= r; j++)
+            {
+                if (Math.Sqrt(j * j + i * i) <= r)
+                    Console.Write("* ");
+                else
+                    Console.Write("  ");
+            }
+            Console.WriteLine();
+        }
+    } 
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Lutfen cizim yapilacak dairenin yaricapini giriniz :");
+        try
+        {
+            int r = Convert.ToInt32(Console.ReadLine());
+            DrawingCircle.DrawCircle(r);
+
+        }catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Hatali bir giris yaptiniz...");
+        }
+    }
+}
